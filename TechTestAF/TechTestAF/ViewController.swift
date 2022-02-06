54e5118bb0775793849a6fb5faf6c979f9fecd4c
Abrahán Fernández <afernandez.nieto@atsistemas.com>
2022-02-06 12:22:21 +0100

    Add Cocoapods & SwiftLint to project.
    - Add .swiflint.yml to custom rules
    - Add .gitignore to repo

---
+++ TechTestAF/TechTestAF/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TechTestAF
+//
+//  Created by Abrahán Fernández on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
